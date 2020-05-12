2597190304fb452ab2625ee3cb790d851e087a8b
JoacoGirod <joaquingirod@gmail.com>
2020-05-12 19:11:14 -0300

    lala

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var controller = require("../controllers/usercontroller")
 /* GET users listing. */
+router.get("/new", controller.newUser)
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
